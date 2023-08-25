40f747dc25b5820e44b66e1188d83c284e9080af
Sumit Sharma <sumit110025@gmail.com>
2023-08-25 14:30:08 +0530

    populate is fixed

---
+++ config/plugins.js
@@ -0,0 +1,13 @@
+// ~/strapi-aws-s3/backend/config/plugins.js
+
+module.exports = ({ env }) => ({
+  transformer: {
+    enabled: true,
+    config: {
+      responseTransforms: {
+        removeAttributesKey: true,
+        removeDataKey: true,
+      },
+    },
+  },
+});
