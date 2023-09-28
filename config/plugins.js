be5b91fbc7e2851f10a8d94df57e06b53ffd10c5
Quentin Vidal <33239211+qisforq@users.noreply.github.com>
2023-09-27 21:07:34 -0400

    updated author and added email plugin

--- config/plugins.js
+++ config/plugins.js
@@ -10,4 +10,18 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  email: {
+    config: {
+      provider: 'sendgrid', // For community providers pass the full package name (e.g. provider: 'strapi-provider-email-mandrill')
+      providerOptions: {
+        apiKey: env('SENDGRID_API_KEY'),
+      },
+      settings: {
+        defaultFrom: 'hello@vidalco.in',
+        defaultReplyTo: 'hello@vidalco.in',
+      },
+    },
+  },
 });
+
+
