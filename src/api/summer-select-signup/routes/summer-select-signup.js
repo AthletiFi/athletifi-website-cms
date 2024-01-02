c95f22f824f80c67f47f2e76aea731e91042db66
Hector Agudelo <haacny86@gmail.com>
2024-01-01 21:01:32 -0500

    created the collection for the summer select form

---
+++ src/api/summer-select-signup/routes/summer-select-signup.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * summer-select-signup router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::summer-select-signup.summer-select-signup');
---
+++ src/api/summer-select-signup/services/summer-select-signup.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * summer-select-signup service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::summer-select-signup.summer-select-signup');
