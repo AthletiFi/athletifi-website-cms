16625b82eefc066bd2a7dcb988941d83f8e9320b
Quentin Vidal <33239211+qisforq@users.noreply.github.com>
2024-01-03 00:18:27 -0500

    added domains

--- config/middleware.js
+++ config/middleware.js
@@ -2,7 +2,7 @@ module.exports = {
     settings: {
       cors: {
         enabled: true,
-        origin: ['http://localhost:3000'], // replace with the domain of your Next.js application
+        origin: ['http://localhost:3000','https://athleti.fi','https://athletifiselect.com'], // replace with the domain of your Next.js application
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS', 'HEAD'], // allowed HTTP methods
         headers: ['Content-Type', 'Authorization', 'X-Frame-Options'], // allowed headers
       },
