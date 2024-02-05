348a8f4b2c3087fe0968f4193c4b5ce6f9c77064
Hector Agudelo <haacny86@gmail.com>
2024-02-05 17:19:26 -0500

    email reminder script working. using cron set at 5 sec intervals for testing

--- config/cronTimer.js
+++ config/cronTimer.js
@@ -1,16 +1,16 @@
-// // Path: ./config/functions/bootstrap.js
+// Path: ./config/functions/bootstrap.js
 
-// const cron = require('node-cron');
-// const {sendPaymentReminders} = require('../src/api/summer-select-signup/services/sendPaymentReminder'); // Add missing import statement
+const cron = require('node-cron');
+const {sendPaymentReminders} = require('../src/api/summer-select-signup/services/sendPaymentReminder'); // Add missing import statement
 
 
-// module.exports = async () => {
-//     cron.schedule('*/5 * * * * *', async () => {
-//         console.log('Cron job running');
-//         // This will run every 5 days at 00:00.
-//         await sendPaymentReminders();
-//     });
-// };
+module.exports = async () => {
+    cron.schedule('*/5 * * * * *', async () => {
+        console.log('Cron job running');
+        // This will run every 5 days at 00:00.
+        await sendPaymentReminders();
+    });
+};
 
 // seconds for testing '*/5 * * * * *'
 // 5 days '0 0 */5 * *'
--- src/api/summer-select-signup/services/sendPaymentReminder.js
+++ src/api/summer-select-signup/services/sendPaymentReminder.js
@@ -1,28 +1,30 @@
-// module.exports = {
-//     async sendPaymentReminders() {
-//         // console.log(Object.keys(strapi.services));
-//     console.log('sendPaymentReminders called');
-//     const summerSelectSignupService = strapi.query('');
-//     console.log(summerSelectSignupService);
-//     const users = await summerSelectSignupService.find({ stripePaid: false });
-//     console.log('Users to send reminders to:', users);
+module.exports = {
+    async sendPaymentReminders() {
+        // console.log(Object.keys(strapi.services));
+    console.log('sendPaymentReminders called');
+    const summerSelectSignupService = strapi.query('api::summer-select-signup.summer-select-signup');
+    console.log(summerSelectSignupService);
+    const users = await summerSelectSignupService.findMany({
+        where: { stripePaid: false }, // Adjust based on actual fields
+    });
+    console.log('Users to send reminders to:', users);
   
-//       for (const user of users) {
-//         const emailContent = {
-//           to: user.email,
-//           from: `AthletiFi Select <${process.env.SENDGRID_DEFAULT_EMAIL_FROM}>`,
-//           replyTo: process.env.SENDGRID_DEFAULT_EMAIL_REPLY_TO,
-//           subject: "need to pay",
-//           text: `Hello ${user.parentFirstName},
+      for (const user of users) {
+        const emailContent = {
+          to: user.email,
+          from: `AthletiFi Select <${process.env.SENDGRID_DEFAULT_EMAIL_FROM}>`,
+          replyTo: process.env.SENDGRID_DEFAULT_EMAIL_REPLY_TO,
+          subject: "need to pay",
+          text: `Hello ${user.parentFirstName},
   
-//           pay now: ${user.parentFirstName}`,
-//         };
+          pay now: ${user.parentFirstName}`,
+        };
   
-//         try {
-//           await strapi.plugins['email'].services.email.send(emailContent);
-//           console.log(`Payment reminder sent to ${user.email}`);
-//         } catch ({ response }) {
-//           console.log(response.body);
-//         }
-//       }
-//   }}
+        try {
+          await strapi.plugins['email'].services.email.send(emailContent);
+          console.log(`Payment reminder sent to ${user.email}`);
+        } catch ({ response }) {
+          console.log(response.body);
+        }
+      }
+  }}
