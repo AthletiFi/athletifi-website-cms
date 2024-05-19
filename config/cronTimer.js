b1bc0a4a5b2640250eeb3e76db5badd9bc15509c
Quentin Vidal <33239211+qisforq@users.noreply.github.com>
2024-05-19 01:19:59 -0400

    feat(email): schedule info session email
    - Add `sendInfoSessionEmail` function in `sendEmails.js` to send info session emails to paid users
    - Update `bootstrap.js` to include a cron job that runs every 15 seconds for testing purposes
    - Ensure dynamic fetching of users who have paid
    - Maintain the existing payment reminder functionality in `sendEmails.js`
    
    BREAKING CHANGE: The `sendPaymentReminder.js` file has been removed and replaced with `sendEmails.js`. Please update your imports and references accordingly.

--- config/cronTimer.js
+++ config/cronTimer.js
@@ -1,18 +1,23 @@
 // Path: ./config/functions/bootstrap.js
 
 const cron = require('node-cron');
-const {sendPaymentReminders} = require('../src/api/summer-select-signup/services/sendPaymentReminder');
-
+const {sendPaymentReminder, sendInfoSessionEmail} = require('../src/api/summer-select-signup/services/sendEmails');
 
 module.exports = async () => {
   if (process.env.SERVE_MODE) {
-    
     // cron.schedule('14 11 6,14,22,29 * *', async () => { // Runs at 11:14 AM UTC on the 6th, 14th, 22nd, and 29th of every month.
     cron.schedule('14 11 * * *', async () => { // Runs every day at 11:14 AM UTC.
     // cron.schedule('*/10 * * * * *', async () => { // Runs every 10 seconds
       console.log(`[${new Date().toISOString()}] Cron job running! Time to CRON!!!`);
-      await sendPaymentReminders();
+      await sendPaymentReminder();
     });
+
+    // Schedule the new email to be sent on May 20th, 2024 at 11:00 AM UTC
+    cron.schedule('0 11 20 5 *', async () => {
+      console.log(`[${new Date().toISOString()}] Sending info session email! `);
+      await sendInfoSessionEmail();
+    });
+
     console.log(`[${new Date().toISOString()}] Cron scheduler initialized successfully. This is because SERVE_MODE is set to ${process.env.SERVE_MODE}`);
   }
 };
